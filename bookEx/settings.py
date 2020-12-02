938e95b5b394a3e50c44ff4cbd637ce1a70d45b5
Bronko671 <69820662+Bronko671@users.noreply.github.com>
2020-12-01 18:52:01 -0800

    Update settings.py

--- bookEx/settings.py
+++ bookEx/settings.py
@@ -29,6 +29,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# THIS IS FOR THE EMAIL
+# DEFAULT_FROM_EMAIL = 'bookexproject@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bookexproject@gmail.com'
+EMAIL_HOST_PASSWORD = 'Clkrss3337'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 
 # Application definition
 
